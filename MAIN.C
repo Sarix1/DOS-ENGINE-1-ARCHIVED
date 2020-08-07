8d133ac711d77c7d518dbb4b9f6f7799bb6767ab
Sarix1 <sari.katariina.jokinen@protonmail.com>
2020-08-07 16:10:24 +0300

    Separate video and graphics functions, test both in main

--- MAIN.C
+++ MAIN.C
@@ -1,11 +1,28 @@
 #include "TEXT.H"
+#include "VIDEO.H"
+#include "GFX_BASIC.H"
 
 int main()
 {
+	// Hello
 	resetText();
 	putText("Hello", FG+BLUE, BG+BLACK, 0, 0);
 	putText("World", FG+RED, BG+BLACK, 2, 1);
 	setTextColor(0, 0);
 	
+	// Initialize video
+	initVideo();
+	// initArrays();
+	drawTarget = offScreen;
+	
+	// Test draw
+	drawBoxCenter(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, 50, 50, 4);
+	updateBuffer();
+	
+	// Wait for user before exit
+	getch();
+	
+	// Exit
+	setVideo(TEXT_MODE);
 	return 0;
 }
